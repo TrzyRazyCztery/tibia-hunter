3ccf807b1407d9c34e48f42dae23a5640711392a
Marcin Grzywaczewski <killavus@gmail.com>
2018-10-14 03:08:16 +0200

    Initial version of hunts list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'normalize.css';
+
+import "./style.css";
+import { huntsFixture } from "./fixtures";
+import crownTransparent from "./crown-transparent.png";
+
+const playerIcons = {
+  ROYAL_PALADIN: "https://vignette.wikia.nocookie.net/tibia/images/0/06/Chain_Bolter.gif/revision/latest?cb=20080619203720&path-prefix=en",
+  ELDER_DRUID: "https://vignette.wikia.nocookie.net/tibia/images/8/8f/Glacial_Rod.gif/revision/latest?cb=20120622174236&path-prefix=en",
+  ELITE_KNIGHT: "https://vignette.wikia.nocookie.net/tibia/images/e/e8/Crystalline_Axe.gif/revision/latest?cb=20120622190625&path-prefix=en",
+  MASTER_SORCERER: "https://vignette.wikia.nocookie.net/tibia/images/4/4a/Wand_of_Darkness.gif/revision/latest?cb=20170725204545&path-prefix=en"
+};
+
+const vocationShorts = {
+  ROYAL_PALADIN: "RP",
+  ELDER_DRUID: "ED",
+  ELITE_KNIGHT: "EK",
+  MASTER_SORCERER: "MS"
+};
+
+const ordering = ["ELITE_KNIGHT", "ELDER_DRUID", "ROYAL_PALADIN", "MASTER_SORCERER"];
+
+const sortCharacters = (characters) => characters.sort((character1, character2) => ordering.indexOf(character1.vocation) > ordering.indexOf(character2.vocation));
+
+const CharacterBadge = ({ level, name, vocation, partyLeader }) => (
+  <a href="https://www.tibia.com/community/?subtopic=characters&name=Sajgonka+Orientu" className="player-badge">
+    <img src={playerIcons[vocation]} className="vocation-image" alt="" />
+    <strong>{name}</strong> , {level} {vocationShorts[vocation]} {partyLeader && <img src={crownTransparent} alt="Party Leader" title="This character is party leader." />}
+  </a>
+);
+
+const HuntEntry = ({ id, characters }) => (
+  <li>
+    <div className="actions">
+      <a href={`hunt/${id}`} className="button primary">View</a>
+      <a href={`hunt/${id}`} className="button secondary">Archive</a>
+    </div>
+
+    <hr />
+    {sortCharacters(characters).map((character) => <CharacterBadge key={character.id} {...character} />)}
+  </li>
+);
+
+const HuntsList = ({ hunts }) => (
+  <ul className="hunts-list">
+    {hunts.map((hunt) => <HuntEntry key={hunt.id} {...hunt} />)}
+  </ul>
+);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { hunts: null };
+  }
+
+  componentDidMount() {
+    huntsFixture().then((data) => {
+      this.setState({ hunts: data });
+    });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    const { hunts } = this.state;
+
+    return hunts === null ? <p>Loading...</p> : (
+      <div className="main-page">
+        <h1>My Hunts</h1>
+        <HuntsList hunts={hunts} />
       </div>
     );
   }
---
+++ src/fixtures.js
@@ -0,0 +1,64 @@
+const characters = {
+  SAJGONKA: {
+    id: "2b2d7023-3b27-44a2-85fb-6d857829af56",
+    name: "Sajgonka Orientu",
+    vocation: "ELDER_DRUID",
+    level: 357
+  },
+  SANCTALUX: {
+    id: "1dd4e111-a8cf-449e-bc4b-2b21c4ea2e4b",
+    name: "Sancta Lux",
+    vocation: "ROYAL_PALADIN",
+    level: 366
+  },
+  HENLOTH: {
+    id: "4b247505-40d6-4a3d-a7db-a256b2347d48",
+    name: "Henloth",
+    vocation: "MASTER_SORCERER",
+    level: 363
+  },
+  PRALIDOKSYM: {
+    id: "acce6f31-fb52-4df3-9245-a4f884339949",
+    name: "Elitarny Pralidoksym",
+    vocation: "ELITE_KNIGHT",
+    level: 388
+  },
+  TANKMCLOVIN: {
+    id: "6df59975-39b5-45fa-a839-a2741d0f6aeb",
+    name: "Tank McLovin",
+    vocation: "ELITE_KNIGHT",
+    level: 460
+  }
+};
+
+const hunts = {
+  HUNT_1: {
+    id: "493eb9b8-a807-4352-aaf7-385a11f7885c",
+    characters: [
+      { ...characters["SAJGONKA"], partyLeader: true },
+      characters["HENLOTH"],
+      characters["SANCTALUX"],
+      characters["PRALIDOKSYM"]
+    ]
+  },
+  HUNT_2: {
+    id: "dba72623-6a0a-4851-93b1-4ede30acb8c0",
+    characters: [
+      characters["SAJGONKA"],
+      { ...characters["PRALIDOKSYM"], partyLeader: true }
+    ]
+  },
+  HUNT_3: {
+    id: "e3ec15df-814e-4c2f-9c8a-ce47527da7a3",
+    characters: [
+      characters["PRALIDOKSYM"],
+      { ...characters["SANCTALUX"], partyLeader: true }
+    ]
+  }
+};
+
+const makeFixture = (fixture) => () => new Promise((resolve) => {
+  return resolve(fixture);
+});
+
+export const huntsFixture = makeFixture([hunts["HUNT_1"], hunts["HUNT_2"], hunts["HUNT_3"]]);
